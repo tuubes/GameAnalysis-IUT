a462fb18159679a971f8cbb17a18ea236f74b847
Guillaume R <theelectronwill@gmail.com>
2018-04-01 22:43:46 +0200

    PreparedStatement pour MoveListener

--- src/main/java/fr/iut2/stid/gameanalysis/MoveListener.java
+++ src/main/java/fr/iut2/stid/gameanalysis/MoveListener.java
@@ -1,16 +1,26 @@
 package fr.iut2.stid.gameanalysis;
 
-import org.bukkit.entity.Player;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 /**
  * Listener pour les déplacements des joueurs et entités.
+ * 
  * @author guillaume
  */
 public class MoveListener implements Listener {
 	
+	private PreparedStatement insertMoveEvent;
+	
+	public MoveListener(Connection conn) throws SQLException {
+		insertMoveEvent = conn.prepareStatement("INSERT INTO PlayerMoves VALUES(?,?,?,?)");
+	}
+
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent evt) {
 		Player p = evt.getPlayer();
