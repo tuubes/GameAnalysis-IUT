2ed236c7b0fd43d489407170c4b9d95bb47a7beb
Unknown <rayan.moissonnier0@orange.fr>
2018-04-01 22:56:50 +0200

    Suvegarde en base de donnée taille message

--- src/main/java/fr/iut2/stid/gameanalysis/MessageListener.java
+++ src/main/java/fr/iut2/stid/gameanalysis/MessageListener.java
@@ -1,15 +1,34 @@
-package fr.iut2.stid.gameanalysis; 
- 
+package fr.iut2.stid.gameanalysis;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.java.JavaPlugin; 
- 
-public class MessageListener implements Listener { 
- 
-  @EventHandler
-  public void onPlayerMessage(AsyncPlayerChatEvent evt) { 
-    int nbmsg = evt.getMessage().length();
-    
-    
-  } 
+
+public class MessageListener implements Listener {
+	private PreparedStatement insertMessageEvent;
+
+	public MessageListener(Connection conn) throws SQLException {
+		insertMessageEvent = conn.prepareStatement("INSERT INTO Messages VALUES(?)");
+	}
+
+	@EventHandler
+	public void onPlayerMessage(AsyncPlayerChatEvent evt) {
+		int nbmsg = evt.getMessage().length();
+		
+		try {
+			insertMessageEvent.setInt(1, nbmsg);
+			insertMessageEvent.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+
+}
