d84905656ed0395b367dca8a5c4ed7e6dcc9b1b3
Guillaume R <theelectronwill@gmail.com>
2018-04-02 00:46:09 +0200

    Enregistrement des déplacements inter-tronçons des joueurs
    Seuls les changements de tronçon (chunk) sont enregistrés, pas les déplacements intra-tronçons.

--- src/main/java/fr/iut2/stid/gameanalysis/MoveListener.java
+++ src/main/java/fr/iut2/stid/gameanalysis/MoveListener.java
@@ -3,26 +3,72 @@ package fr.iut2.stid.gameanalysis;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 /**
- * Listener pour les déplacements des joueurs et entités.
+ * Listener pour les déplacements des joueurs et des entités non-joueurs.
  * 
  * @author guillaume
  */
 public class MoveListener implements Listener {
+
+	private final PreparedStatement insertPlayerMove;
 	
-	private PreparedStatement insertMoveEvent;
-	
+	/** Associe chaque entité à un objet EntityChunkInfos qui permet de détecter quand l'entité change de chunk */
+	private final Map<Entity, EntityChunkInfos> chunkInfos = new HashMap<>();
+
 	public MoveListener(Connection conn) throws SQLException {
-		insertMoveEvent = conn.prepareStatement("INSERT INTO PlayerMoves VALUES(?,?,?,?)");
+		insertPlayerMove = conn.prepareStatement("INSERT INTO PlayerMoves VALUES(?,?,?,?)");
 	}
 
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent evt) {
-		Player p = evt.getPlayer();
+		Player player = evt.getPlayer();
+		Location newLocation = player.getLocation();
+		EntityChunkInfos infos = chunkInfos.computeIfAbsent(player, EntityChunkInfos::new);
+		if (infos.update(newLocation)) {
+			long time = System.currentTimeMillis();
+			try {
+				insertPlayerMove.setLong(1, time);
+				insertPlayerMove.setInt(2, infos.lastChunkX);
+				insertPlayerMove.setInt(3, infos.lastChunkY);
+				insertPlayerMove.setInt(4, infos.lastChunkZ);
+				insertPlayerMove.executeUpdate();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	private class EntityChunkInfos {
+		int lastChunkX, lastChunkY, lastChunkZ;
+
+		EntityChunkInfos(Entity e) {
+			Location l = e.getLocation();
+			lastChunkX = l.getBlockX() / 16;
+			lastChunkY = l.getBlockY() / 16;
+			lastChunkZ = l.getBlockZ() / 16;
+		}
+
+		boolean update(Location newLocation) {
+			int newChunkX = newLocation.getBlockX() / 16;
+			int newChunkY = newLocation.getBlockY() / 16;
+			int newChunkZ = newLocation.getBlockZ() / 16;
+			boolean changed = (newChunkX != lastChunkX) || (newChunkY != lastChunkY) || (newChunkZ != lastChunkZ);
+			if (changed) {
+				lastChunkX = newChunkX;
+				lastChunkY = newChunkY;
+				lastChunkZ = newChunkZ;
+			}
+			return changed;
+		}
 	}
 }
