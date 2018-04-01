26147867cac1469b5a17dcb3f9a357f3e3290cf7
Unknown <alexandre7494@gmail.com>
2018-04-01 23:51:03 +0200

    Sauvegarde du type de bloc placé

---
+++ src/main/java/fr/iut2/stid/gameanalysis/BlockPlaceListener.java
@@ -0,0 +1,34 @@
+package fr.iut2.stid.gameanalysis;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockPlaceListener implements Listener {
+	private PreparedStatement insertBlockPlaceEvent;
+	
+	public BlockPlaceListener(Connection conn) throws SQLException {
+		insertBlockPlaceEvent = conn.prepareStatement("INSERT INTO PlacedBlocks VALUES(?,?)");
+	}
+	@EventHandler
+	public void onPlayerBlockPlaced(BlockPlaceEvent evt) {
+		Player p = evt.getPlayer();
+		int b  = evt.getBlockPlaced().getTypeId();
+		
+		long ticks = p.getStatistic(Statistic.PLAY_ONE_TICK);
+		try { 
+			 insertBlockPlaceEvent.setInt(1, b);
+			 insertBlockPlaceEvent.setLong(1, ticks);
+			 insertBlockPlaceEvent.executeUpdate(); 
+		    } catch (SQLException e) { 
+		      // TODO Auto-generated catch block 
+		      e.printStackTrace(); 
+		    } 
+	}
+}
