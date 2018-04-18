256ce3415800a0a893ca457c7668f525af4bd49e
Guillaume R <theelectronwill@gmail.com>
2018-04-18 18:09:04 +0200

    Ajout du test R<->H2

---
+++ analysis/test.R
@@ -0,0 +1,42 @@
+library(RJDBC)
+
+dbPath <-
+  "jdbc:h2:/home/guillaume/Documents/Minecraft/serv_a/plugins/GameAnalysisIUT2/database"
+
+print("loading h2")
+drv <-
+  JDBC(driverClass = "org.h2.Driver", classPath = "/home/guillaume/h2.jar", identifier.quote="`")
+
+print("connecting to the database")
+conn2 <- dbConnect(drv, dbPath, "", "")
+
+print("executing requests")
+dbListTables(conn2)
+
+t <- dbReadTable(conn2, "PLAYERMOVES")
+class(t)
+
+dbGetQuery(conn2, "SELECT * FROM MESSAGES")
+dbGetQuery(conn2, "SELECT * FROM Messages")
+dbGetQuery(conn2, "SELECT * FROM BrokenBlocks")
+dbGetQuery(conn2, "SELECT * FROM PlayerMoves")
+
+print("disconnecting")
+dbDisconnect(conn2)
+
+
+library(RH2)
+print("loading h2")
+h2 <- H2(jars = "/home/guillaume/h2.jar")
+
+print("connecting to the database")
+conn <- dbConnect(h2, dbPath, "", "")
+
+print("executing requests")
+dbGetQuery(conn, "SHOW TABLES")
+
+dbGetQuery(conn, "SELECT * FROM MESSAGES")
+dbGetQuery(conn, "SELECT * FROM PlayerMoves")
+
+print("disconnecting")
+dbDisconnect(conn)
