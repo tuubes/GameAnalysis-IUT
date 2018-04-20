aff64fa77efd7e80e56bf3ba2e048b98017e211d
Guillaume R <theelectronwill@gmail.com>
2018-04-20 16:22:35 +0200

    Amélioration mineure du message informant de la collecte de données
    - Correction des espaces
    - Ajout de couleurs

--- plugin/src/main/java/fr/iut2/stid/gameanalysis/PrivacyInformer.java
+++ plugin/src/main/java/fr/iut2/stid/gameanalysis/PrivacyInformer.java
@@ -1,18 +1,21 @@
 package fr.iut2.stid.gameanalysis;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * Informe les joueurs que le plugin récolte des données.
+ *
  * @author guillaume
  */
 public class PrivacyInformer implements Listener {
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent evt) {
-		evt.getPlayer().sendMessage("En jouant sur ce serveur, vous acceptez que des informations concernant vos actions"
-				+ "dans le jeu soient enregistrées et analysées pour aider le projet open-source Tuubes (http://tuubes.org)."
+		evt.getPlayer().sendMessage("En jouant sur ce serveur, vous acceptez que des informations concernant vos actions "
+				+ "dans le jeu soient enregistrées et analysées pour aider le projet open-source Tuubes "
+				+ ChatColor.AQUA + "(http://tuubes.org). " + ChatColor.RESET
 				+ "Nous ne collecterons jamais votre adresse IP ni le contenu de vos messages.");
 	}
 }
