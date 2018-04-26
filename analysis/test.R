07fcf83add9e813b38a783a72a8c794f039cd2d4
Guillaume R <guillaumeraffin@free.fr>
2018-04-26 16:25:03 +0200

    Explication du test.R

--- analysis/test.R
+++ analysis/test.R
@@ -1,28 +1,39 @@
+# Chargement du package RJDBC
 library(RJDBC)
 
-dbPath <-
-  "jdbc:h2:/home/guillaume/Documents/Minecraft/serv_a/plugins/GameAnalysisIUT2/database"
+# Chemin de la base de données, sans le .mv.db à la fin
+dbPath <- "/home/guillaume/Documents/Minecraft/serv_a/plugins/GameAnalysisIUT2/database"
+# Chemin du SGBD (DBMS en anglais) .jar
+dbmsPath <- "/home/guillaume/h2.jar"
 
+# Chargement du pilote de la base de données
 print("loading h2")
 drv <-
-  JDBC(driverClass = "org.h2.Driver", classPath = "/home/guillaume/h2.jar", identifier.quote="`")
+  JDBC(driverClass = "org.h2.Driver", classPath = dbmsPath, identifier.quote="`")
 
+# Connexion à la base de données. Attention ! Si elle n'existe pas, une BDD vide sera crée.
 print("connecting to the database")
-conn2 <- dbConnect(drv, dbPath, "", "")
+conn <- dbConnect(drv, "jdbc:h2:" + dbPath, "", "") # On récupère un objet conn qui nous servira par la suite
 
+# --- Utilisation de la BDD ---
 print("executing requests")
-dbListTables(conn2)
+dbListTables(conn) # Liste les tables de la BDD (y compris les tables crées par le SGBD et qui ne contiennent pas nos données)
+dbGetQuery(conn, "SHOW TABLES") # Liste les tables de la BDD (uniquement celles que nous avons créées)
 
-t <- dbReadTable(conn2, "PLAYERMOVES")
-class(t)
+# Lecture d'une table entière de la BDD
+data <- dbReadTable(conn, "PLAYERMOVES")
+class(data) # On obtient un data frame avec le nom des colonnes
+View(data)
 
-dbGetQuery(conn2, "SELECT * FROM MESSAGES")
-dbGetQuery(conn2, "SELECT * FROM Messages")
-dbGetQuery(conn2, "SELECT * FROM BrokenBlocks")
-dbGetQuery(conn2, "SELECT * FROM PlayerMoves")
+# On peut aussi exécuter des requêtes SQL avec la fonction dbGetQuery
+dbGetQuery(conn, "SELECT * FROM MESSAGES")
+dbGetQuery(conn, "SELECT * FROM Messages")
+dbGetQuery(conn, "SELECT * FROM BrokenBlocks")
+dbGetQuery(conn, "SELECT * FROM PlayerMoves")
 
+# Une fois toutes les requêtes effectuées, il faut se déconnecter de la base de données avec dbDisconnect
 print("disconnecting")
-dbDisconnect(conn2)
+dbDisconnect(conn)
 
 
 
