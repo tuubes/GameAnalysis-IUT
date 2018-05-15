57ff940c77e7ecb123c2cac4d17676354312cea1
Unknown <rayan.moissonnier0@orange.fr>
2018-05-15 12:44:16 +0200

    Correction analyse_message2

--- analysis/analyse_message2.R
+++ analysis/analyse_message2.R
@@ -2,13 +2,13 @@
 # Chargement du package RJDBC
 library(RJDBC)
 
-# --- ParamÃÂÃÂ¨tres---
-dbPath <- "C:/Users/utilisateur/Desktop/Cours/Projet Tutoré/database_bofas" # Chemin de la base de données, sans le .mv  la fin
-dbmsPath <- "C:/Users/utilisateur/Desktop/Cours/Projet Tutoré/h2" # Chemin du SGBD (DBMS en anglais) .jar
+# --- Paramètres---
+dbPath <- "C:/Users/utilisateur/Desktop/Cours/Projet Tutoré/database_bofas" # Chemin de la base de données, sans le .mv la fin
+dbmsPath <- "C:/Users/utilisateur/Desktop/Cours/Projet Tutoré/h2.jar" # Chemin du SGBD (DBMS en anglais) .jar
 
 # --- Connexion à la BDD ---
 drv <- JDBC(driverClass = "org.h2.Driver", classPath = dbmsPath, identifier.quote="`")
-conn <- dbConnect(drv, paste("jdbc:h2:", dbPath, sep=""), "", "") # On récupère¨re un objet conn qui nous servira par la suite
+conn <- dbConnect(drv, paste("jdbc:h2:", dbPath, sep=""), "", "") # On récupère un objet conn qui nous servira par la suite
 
 # --- Analyses statistiques ---
 data <- dbGetQuery(conn, "SELECT Size FROM Messages")
